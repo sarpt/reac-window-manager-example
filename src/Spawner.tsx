acaa22aefc0c094cb0b21853c5fa1005f5300e25
sarpt <mike.m.info@gmail.com>
2022-03-06 12:48:16 +0100

    Some small changes to window creation
    Removed unneccessary repetition of passing instance key to windows creators - from now on open receives window creator which decides what to do with it, if anything.

--- src/Spawner.tsx
+++ src/Spawner.tsx
@@ -1,18 +1,18 @@
 import React, { useCallback, useContext } from 'react';
-import { WindowManagerContext, window } from './WindowManager';
+import { WindowManagerContext, windowCreator } from './WindowManager';
 
 import { RedDialog } from './RedDialog';
 import { BlueDialog } from './BlueDialog';
 
-const redWindow: window = {
-  body: (instanceKey, props) => <RedDialog instanceKey={instanceKey} {...props}/>,
-  header: (instanceKey) => <span>The red dialog: {instanceKey}</span>,
-};
+const redWindowCreator: windowCreator = (instanceKey) => ({
+  body: (props) => <RedDialog instanceKey={instanceKey} {...props}/>,
+  header: () => <span>The red dialog: {instanceKey}</span>,
+});
 
-const blueWindow: window = {
-  body:  (instanceKey) => <BlueDialog instanceKey={instanceKey}/>,
-  header: (instanceKey) => <span>Some title: {instanceKey}</span>,
-};
+const blueWindowCreator: windowCreator = (instanceKey) => ({
+  body:  () => <BlueDialog instanceKey={instanceKey}/>,
+  header: () => <span>Some title: {instanceKey}</span>,
+});
 
 export const Spawner = () => {
   const windowsManager = useContext(WindowManagerContext);
@@ -29,8 +29,8 @@ export const Spawner = () => {
         windowsManager 
           ? (
             <>
-              <button onClick={() => windowsManager.open(redWindow) }>Show red dialog</button>
-              <button onClick={() => windowsManager.open(blueWindow) }>Show blue dialog</button>
+              <button onClick={() => windowsManager.open(redWindowCreator) }>Show red dialog</button>
+              <button onClick={() => windowsManager.open(blueWindowCreator) }>Show blue dialog</button>
             </>
           )
           : <></>
